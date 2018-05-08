abc89390a4c16b2a741edc63765c712c7df5bd42
Bruno Krebs <bruno.krebs@auth0.com>
2018-05-08 13:45:52 -0300

    moving worker and integrating it with RabbitMQ;

---
+++ worker/src/index.js
@@ -0,0 +1,46 @@
+const Config = require('../config.json');
+const rp = require('request-promise');
+const rabbit = require('amqplib').connect(Config.RABBIT_URL);
+
+async function consumeTasks() {
+  try {
+    const conn = await rabbit;
+    const channel = await conn.createChannel();
+    await channel.assertQueue('bookmarks');
+
+    channel.consume('bookmarks', (bookmark) => {
+      checkBookmark(JSON.parse(bookmark.content.toString()));
+      channel.ack(bookmark);
+    });
+  } catch (err) {
+    console.warn(err);
+    // keep going
+    consumeTasks();
+  }
+}
+
+async function checkBookmark(bookmark) {
+  console.log(bookmark);
+  const options = {
+    method: 'PATCH',
+    uri: Config.BOOKMARKS_API,
+    body: {
+      id: bookmark.id,
+    },
+    json: true,
+  };
+
+  rp(bookmark.url)
+    .then(() => patchBookmark(bookmark, true))
+    .catch(() => patchBookmark(bookmark, false));
+}
+
+function patchBookmark(bookmark, status) {
+  options.body.is_ok = true;
+  rp(options).then(() => {
+    console.log(`${bookmark.url} patched as ${status ? 'available' : 'not available'}`);
+  }).catch(console.log);
+}
+
+consumeTasks();
+
