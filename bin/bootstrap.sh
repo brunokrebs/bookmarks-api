9fb1fca9ed0c8b01da492d9d86d85ee5d4602c17
Bruno Krebs <bruno.krebs@auth0.com>
2018-05-08 19:48:54 -0300

    updating bootstrap.sh

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,7 +3,8 @@
 # create symlink to bookmarks-service
 SERVICE_FILE=/etc/systemd/system/bookmarks.service
 if [ ! -f $SERVICE_FILE ]; then
-    ln -s ~/bookmarks-api/bin/bookmarks.service $SERVICE_FILE
+    rm $SERVICE_FILE
+    cp ~/bookmarks-api/bin/bookmarks.service $SERVICE_FILE
 fi
 
 # move into project root
