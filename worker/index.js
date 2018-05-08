e8a54b9c1396eceb6b9a6be6c6fbd61f1d8a2c91
Bruno Krebs <bruno.krebs@auth0.com>
2018-05-08 12:55:51 -0300

    second version of the worker;

--- worker/index.js
+++ worker/index.js
@@ -1,16 +1,33 @@
 const rp = require('request-promise');
 
 const bookmarks = [
-  {name: 'Auth0', url: 'https://auth0.com'},
-  {name: 'Google', url: 'https://www.google.com'},
-  {name: 'My Favorite Place', url: 'https://non-existent.com.br'},
-  {name: 'Amazon', url: 'https://www.amazon.com'},
+  {id :1638, url: 'https://www.amazon.com'},
+  {id :1639, url: 'https://non-existent.com.br'},
+  {id :1640, url: 'https://auth0.com'},
+  {id :1645, url: 'https://www.google.com'},
 ];
 
-bookmarks.forEach((bookmark) => {
+const apiUrl = 'http://ec2-18-218-232-47.us-east-2.compute.amazonaws.com:3000/api/bookmarks';
+
+bookmarks.forEach(async (bookmark) => {
+  const options = {
+    method: 'PATCH',
+    uri: apiUrl,
+    body: {
+      id: bookmark.id,
+    },
+    json: true,
+  };
+
   rp(bookmark.url).then(() => {
-    console.log(`${bookmark.url} is ok`);
+    options.body.is_ok = true;
+    rp(options).then(() => {
+      console.log(`${bookmark.url} patched as OK`);
+    }).catch(console.log);
   }).catch(() => {
-    console.error(`${bookmark.url} is not ok`);
+    options.body.is_ok = false;
+    rp(options).then(() => {
+      console.log(`${bookmark.url} patched as NOT OK`);
+    }).catch(console.log);
   });
 });
